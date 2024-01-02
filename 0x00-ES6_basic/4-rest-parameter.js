7620f0a5481520fd6a39f7c631997e43684b7ca2
Tim Ngeno <timngeno424@gmail.com>
2024-01-02 19:51:13 +0300

    calculate the number of args passed using rest syntax

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,3 @@
 export default function returnHowManyArguments(...args) {
-  let total = 1;
-  for (const arg of args) {
-    if (!Number.isNaN(Number(arg))) {
-      total += arg;
-    }
-  }
-  return total;
+  return args.length;
 }
