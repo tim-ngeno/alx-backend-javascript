a1ea212898fae27f71a51381a8949acebf03d02e
Tim Ngeno <timngeno424@gmail.com>
2024-01-04 17:04:13 +0300

    fix typo

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -11,6 +11,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuating slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
