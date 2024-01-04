0332660bcaa217bac085bfcf9343529cc89d0a74
Tim Ngeno <timngeno424@gmail.com>
2024-01-04 18:21:24 +0300

    return default string description

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,22 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  getDescription() {
+    return `[${this._code}]`;
+  }
+
+  toString() {
+    return this.getDescription();
+  }
+}
