672b2949cfb45463d11668005376d4eb9037b492
Tim Ngeno <timngeno424@gmail.com>
2024-01-04 17:03:01 +0300

    extend the Building class

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,16 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuating slowly the ${this._floors} floors`;
+  }
+}
