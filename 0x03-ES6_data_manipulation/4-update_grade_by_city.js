27eaeed266cae8bd50b738fc04ef06eeb17b9547
Tim Ngeno <timngeno424@gmail.com>
2024-01-08 23:50:02 +0300

    fix eslint errors

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
-    .map((student)=> {
-      const gradeObject = newGrades.find(grade => grade.studentId === student.id);
+    .map((student) => {
+      const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
 
       return {
         ...student,
