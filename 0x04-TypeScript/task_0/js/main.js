df6b199aac56fda8869ec6b6f7f9b69ca45c070e
Tim Ngeno <timngeno424@gmail.com>
2024-01-10 14:04:49 +0300

    create a student interface

---
+++ 0x04-TypeScript/task_0/js/main.js
@@ -0,0 +1,14 @@
+var student1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 23,
+    location: 'Minneapolis',
+};
+var student2 = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 24,
+    location: 'Canary',
+};
+var studentsList = [student1, student2];
+console.table(studentsList);
