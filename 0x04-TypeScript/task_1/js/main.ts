5a3069a2d9e6ce80add811f371765c3710f7c9d4
Tim Ngeno <timngeno424@gmail.com>
2024-01-10 15:27:00 +0300

    implement a class StudentClass described through an interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -29,3 +29,53 @@ const director: Directors = {
 
 console.log(director);
 console.log(teacher);
+
+
+// Write a function printTeacher
+interface PrintTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+function printTeacher(firstName: string, lastName: string) {
+    const firstLetter = firstName.charAt(0).toUpperCase();
+    const fullName = `${firstLetter}. ${lastName}`;
+    return fullName;
+}
+
+const result = printTeacher("John", "Doe");
+console.log(result);
+
+
+
+// Writing a class with an interfaced constructor
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentMethods {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentMethods {
+    firstName: string;
+    lastName: string;
+
+    constructor({ firstName, lastName }: StudentConstructor) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+	return 'Currently working';
+    }
+
+    displayName(): string {
+	return this.firstName
+    }
+}
+
+const studentInstance = new StudentClass({ firstName: 'John', lastName: 'Doe' });
+console.log(studentInstance.workOnHomework());
+console.log(studentInstance.displayName());
