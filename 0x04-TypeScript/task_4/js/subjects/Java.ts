c9e44946e8eb4a29811c767f2fc28ff34103873a
Tim Ngeno <timngeno424@gmail.com>
2024-01-12 10:51:18 +0300

    create a Java class extending from Subject

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,63 @@
+namespace Subjects {
+    export interface Teacher {
+        firstName: string;
+        lastName: string;
+	experienceTeachingC?: number;
+	experienceTeachingReact?: number;
+	experienceTeachingJava?: number;
+    }
+
+    export class Subject {
+        private _teacher: Teacher | undefined;
+
+        setTeacher(teacher: Teacher): void {
+            this._teacher = teacher;
+        }
+
+        get teacher(): Teacher | undefined {
+            return this._teacher;
+        }
+    }
+
+    export class Cpp extends Subject {
+	getRequirements(): string {
+	    return 'Here is the list of requirements for Cpp';
+	}
+
+	getAvailableTeacher(): string {
+	    if (this.teacher && this.teacher.experienceTeachingC) {
+		return `Available Teacher: ${this.teacher.firstName}`;
+	    } else {
+		return `No available teacher`;
+	    }
+	}
+    }
+
+    export class React {
+	getRequirements(): string {
+	    return 'Here is hte list of requirements for React';
+	}
+
+	getAvailableTeacher(): string {
+	    if (this.teacher && this.teacher.experienceTeachingReact) {
+		return `Available Teacher: ${this.teacher.firstName}`;
+	    } else {
+		return 'No available teacher';
+	    }
+	}
+    }
+
+    export class Java {
+	getRequirements(): string {
+	    return 'Here is the list of requirements for Java';
+	}
+
+	getAvailableTeacher(): string {
+	    if (this.teacher && this.teacher.experienceTeachingJava) {
+		return `Available Teacher: ${this.teacher.firstName}`;
+	    } else {
+		return 'No available teacher';
+	    }
+	}
+    }
+}
