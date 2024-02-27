b776ab61925118b1d2e56634b1a7bc4c22407265
Tim Ngeno <timngeno424@gmail.com>
2024-02-27 08:45:20 +0300

    fix typo in Holberton School

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,15 @@
-process.stdout.write('Welcome to Holberton school, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
+
 process.stdin.on('data', (data) => {
   const input = data.trim();
   if (input) {
-    process.stdout.write(`Your name is: ${input}\n`);
+    console.log('Your name is:', input);
     process.exit();
   }
 });
 
 process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
