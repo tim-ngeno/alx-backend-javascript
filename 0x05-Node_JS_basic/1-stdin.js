bfa073618dbc42cc03cd3bc2d0b248989648550d
Tim Ngeno <timngeno424@gmail.com>
2024-02-28 09:11:08 +0300

    wait for asynchronous actions to complete

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,14 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
+process.stdin.once('data', (data) => {
   const input = data.trim();
   if (input) {
     console.log('Your name is:', input);
+    console.log('This important software is now closing');
     process.exit();
   }
-});
-
-process.on('exit', () => {
-  console.log('This important software is now closing');
 });
